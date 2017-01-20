f134a1e63c987651f40aff4f8c95ba765a7d2f06
Oscar Charro Rivera <oschariv@gmail.com>
2017-01-20 13:04:23 +0100

    mejorado contructor Mazo

--- Mazo.java
+++ Mazo.java
@@ -16,36 +16,16 @@ public class Mazo
 
     /**
      * Constructor for objects of class Mazo
+     * El constructor crea el mazo automaticamente (no se incluyen 8 ni 9).
      */
     public Mazo()
     {
         mazo = new ArrayList<Carta>();
         
-        for (int palo = 0; palo <= 3; palo++) {
-            for (int numeracion = 1; numeracion <= 12;numeracion++) {
-                if (((numeracion >=1 && numeracion <=7) || (numeracion >= 10 && 
-                    numeracion <= 12))){
-                    switch (numeracion){
-                        case 1:
-                            Carta elAs = new Carta(1, palo);
-                            mazo.add(elAs);
-                            continue;
-                        case 10:
-                            Carta laSota = new Carta(10, palo);
-                            mazo.add(laSota);
-                            continue;
-                        case 11:
-                            Carta elCaballo = new Carta(11, palo);
-                            mazo.add(elCaballo);
-                            continue;
-                        case 12:
-                            Carta elRey = new Carta(12, palo);
-                            mazo.add(elRey);
-                            continue;
-                        default:
-                            Carta restantes = new Carta(numeracion, palo);
-                            mazo.add(restantes);
-                    }
+        for (int palo = 0; palo <= 3; palo++) {            
+            for (int numeracion = 1; numeracion <= 12;numeracion++) {                
+                if (numeracion != 8 && numeracion != 9) {
+                    mazo.add(new Carta(numeracion, palo));   
                 }
             }
         }
---
+++ MazoTest.java
@@ -0,0 +1,45 @@
+
+
+import static org.junit.Assert.*;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ * The test class MazoTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class MazoTest
+{
+    private Mazo mazo1;
+
+    /**
+     * Default constructor for test class MazoTest
+     */
+    public MazoTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @Before
+    public void setUp()
+    {
+        mazo1 = new Mazo();
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @After
+    public void tearDown()
+    {
+    }
+}
