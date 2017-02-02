8ad220a0e2d2282609e5425ac950869564bee353
Oscar Charro Rivera <oschariv@gmail.com>
2017-02-02 13:29:10 +0100

    añadido metodo cartaQueVaGanandoLaBaza(con errores)

--- Baza.java
+++ Baza.java
@@ -13,11 +13,12 @@ public class Baza
     private int paloQuePinta;
     // Array para guardar las cartas de la baza.
     private Carta[] baza;
-    
+    // Array para guardar nombre de los jugadores.
     private String[] jugadores;
-    
+    // contador fijo para las cartas en la baza.
     private int contadorBaza;
 
+    private Carta cartaGanadora;
     /**
      * Constructor for objects of class Baza
      */
@@ -28,6 +29,7 @@ public class Baza
         baza = new Carta[numeroJugadores];
         jugadores = new String[numeroJugadores];
         contadorBaza = 0;
+        cartaGanadora = null;
     }
 
     /**
@@ -38,10 +40,22 @@ public class Baza
         if (contadorBaza < numeroJugadores){
             baza[contadorBaza] = cartaATirar;
             jugadores[contadorBaza] = nombreJugador;
+
+            Carta cartaComparar = null;
+            if (cartaGanadora == null ) {
+                cartaGanadora = baza[contadorBaza];
+            }
+            else{
+                cartaComparar = baza[contadorBaza];
+                if (cartaComparar.ganaA(cartaGanadora, paloQuePinta)){
+                    cartaGanadora = cartaComparar;
+                }
+            }
+
             contadorBaza++;
         }
     }
-    
+
     /**
      * 
      */
@@ -52,4 +66,14 @@ public class Baza
         }
         return paloPrimeraCarta;
     }
+
+    /**
+     * 
+     */
+    public Carta cartaQueVaGanandoLaBaza(){
+        return cartaGanadora;
+    }
 }
+
+
+
