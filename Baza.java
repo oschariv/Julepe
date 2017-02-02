5b4f3cf3fb32ec07a1b8bf3e65d9026d9dc74a12
Oscar Charro Rivera <oschariv@gmail.com>
2017-02-02 10:43:26 +0100

    creada clase baza

---
+++ Baza.java
@@ -0,0 +1,32 @@
+
+/**
+ * Write a description of class Baza here.
+ * 
+ * @author oschariv(Oscar Charro Rivera) 
+ * @version 1.0(02-01-2017)
+ */
+public class Baza
+{
+    // Indica el numero de jugadores que van a tirar cartas.
+    private int numeroJugadores;
+    // Indica el palo que pinta.
+    private int paloQuePinta;
+
+    /**
+     * Constructor for objects of class Baza
+     */
+    public Baza(int numeroJugadores, int paloQuePinta)
+    {
+        this.numeroJugadores = numeroJugadores;
+        this.paloQuePinta = paloQuePinta;
+    }
+
+    /**
+     * 
+     */
+    public int sampleMethod()
+    {
+        
+        return 1;
+    }
+}
